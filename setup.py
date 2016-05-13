0508fb80d89d2769d84759e9bd24cdc602b2e240
Dvir Volk <dvirsky@gmail.com>
2016-05-13 15:48:00 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='rmtest',
+    version='0.1',
+    description='',
+    url='http://github.com/RedisLabas/rmtest',
+    packages=find_packages(),
+    install_requires=['redis'],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Database',
+        'Topic :: Software Development :: Testing'
+    ]
+)
+
