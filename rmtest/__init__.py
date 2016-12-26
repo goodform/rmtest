a3530eed2267b42635fab32cd27303f8bd2edd22
Dvir Volk <dvirsky@gmail.com>
2016-12-26 12:32:15 +0200

    added env vars

--- rmtest/__init__.py
+++ rmtest/__init__.py
@@ -2,21 +2,29 @@ import unittest
 from .disposableredis import DisposableRedis
 import os
 
-def ModuleTestCase(module_path, redis_path = 'redis-server'):
+REDIS_MODULE_PATH_ENVVAR = 'REDIS_MODULE_PATH'
+REDIS_PATH_ENVVAR = 'REDIS_PATH'
+
+
+def ModuleTestCase(module_path, redis_path='redis-server', fixed_port=None):
+
+    module_path = os.getenv(REDIS_MODULE_PATH_ENVVAR, module_path)
+    redis_path = os.getenv(REDIS_PATH_ENVVAR, redis_path)
 
     class _ModuleTestCase(unittest.TestCase):
+
         _module_path = os.path.abspath(os.path.join(os.getcwd(), module_path))
-        _redis_path =  redis_path
-        
+        _redis_path = redis_path
+
         def redis(self, port=None):
-            return DisposableRedis(port=port, path = self._redis_path, loadmodule = self._module_path)  
-            
+            if fixed_port is not None:
+                port = fixed_port
+            return DisposableRedis(port=port, path=self._redis_path, loadmodule=self._module_path)
+
         def assertOk(self, x):
             self.assertEquals("OK", x)
-        
+
         def assertExists(self, r, key):
-            self.assertTrue(r.exists(key))  
-    
+            self.assertTrue(r.exists(key))
+
     return _ModuleTestCase
-    
-   
