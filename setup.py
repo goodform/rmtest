fbdc95639d11fd8c9466a9fc6444a39e846a8058
Dvir Volk <dvirsky@gmail.com>
2017-01-25 17:40:02 +0200

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rmtest',
-    version='0.1.2',
+    version='0.2',
     
     description='Redis Module Testing Utility',
     url='http://github.com/RedisLabs/rmtest',
