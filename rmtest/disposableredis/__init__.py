77c70bc57a5ca2646a177b1234a6c252f10bdb89
Mark Nunberg <mnunberg@haskalah.org>
2017-06-23 15:43:33 -0700

    disposableredis: formatting

--- rmtest/disposableredis/__init__.py
+++ rmtest/disposableredis/__init__.py
@@ -18,30 +18,30 @@ def get_random_port():
 
 
 class Client(redis.StrictRedis):
-
     def __init__(self, disposable_redis, port):
-
-        redis.StrictRedis.__init__(self, port = port)
+        redis.StrictRedis.__init__(self, port=port)
         self.dr = disposable_redis
 
     def retry_with_rdb_reload(self):
+        yield 1
+        self.dr.dump_and_reload()
+        yield 2
 
-            yield 1
-            self.dr.dump_and_reload()
-            yield 2
 
 class DisposableRedis(object):
 
     def __init__(self, port=None, path='redis-server', **extra_args):
         """
-        :param port: port number to start the redis server on. Specify none to automatically generate
+        :param port: port number to start the redis server on.
+            Specify none to automatically generate
         :type port: int|None
-        :param extra_args: any extra arguments kwargs will be passed to redis server as --key val
+        :param extra_args: any extra arguments kwargs will
+            be passed to redis server as --key val
         """
-
         self._port = port
 
-        # this will hold the actual port the redis is listening on. It's equal to `_port` unless `_port` is None
+        # this will hold the actual port the redis is listening on.
+        # It's equal to `_port` unless `_port` is None
         # in that case `port` is randomly generated
         self.port = None
         self.extra_args = []
@@ -51,14 +51,12 @@ class DisposableRedis(object):
                 self.extra_args += list(v)
             else:
                 self.extra_args.append(v)
-             
-        
+
         self.path = path
         self.dumped = False
         self.errored = False
 
-
-    def _startProcess(self):
+    def _start_process(self):
         #print("Starting redis process: {}".format(' '.join(self.args)))
         self.process = subprocess.Popen(
             self.args,
@@ -76,7 +74,7 @@ class DisposableRedis(object):
                     raise RuntimeError(
                         "Process has exited with code {}\n. Redis output: {}"
                         .format(self.process.returncode, self.process.stdout.read()))
-                
+
                 time.sleep(0.1)
 
     def __enter__(self):
@@ -87,29 +85,28 @@ class DisposableRedis(object):
 
         self.dumpfile = 'dump.%s.rdb' % self.port
         self.args = [self.path,
-                '--port', str(self.port),
-                '--save', '',
-                '--dbfilename', self.dumpfile] + self.extra_args
+                     '--port', str(self.port),
+                     '--save', '',
+                     '--dbfilename', self.dumpfile] + self.extra_args
 
-
-        self._startProcess()
+        self._start_process()
         return self.client()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.process.terminate()
         if exc_val or self.errored:
-            sys.stderr.write("Redis output: {}\n".format(self.process.stdout.read()))
+            sys.stderr.write("Redis output: {}\n".format(
+                self.process.stdout.read()))
         if self.dumped:
             try:
                 os.unlink(self.dumpfile)
             except OSError:
                 pass
-            
+
     def dump_and_reload(self):
         """
         Dump the rdb and reload it, to test for serialization errors
         """
-
         conn = self.client()
         conn.save()
         self.dumped = True
@@ -119,15 +116,8 @@ class DisposableRedis(object):
             self.errored = True
             raise err
 
-        
-
-    
-  
-        
-    
     def client(self):
         """
         :rtype: redis.StrictRedis
         """
-
         return Client(self, self.port)
