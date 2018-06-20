abe0fafd9c9b7b37b8b3c135350df14eb99758fb
Mark Nunberg <mnunberg@haskalah.org>
2018-06-19 22:48:47 -0400

    New version with base class

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rmtest',
-    version='0.6.9',
+    version='0.7.0',
 
     description='Redis Module Testing Utility',
     url='http://github.com/RedisLabs/rmtest',
