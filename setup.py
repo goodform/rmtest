2b82d1b8599e808cff66ebed9335a94d0618cc62
Nathan Scott <nathans@redhat.com>
2018-09-12 14:05:24 +1000

    package: stable 1.0 release for all python versions
    Update setup.py with 1.0 version number, and several
    additional 'classifiers' - esp. for python3 support.
    Add a Red Hat copyright note for my recent changes.

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-
 setup(
     name='rmtest',
-    version='0.7.0',
-
+    version='1.0.0',
     description='Redis Module Testing Utility',
     url='http://github.com/goodform/rmtest',
     packages=find_packages(),
     install_requires=['redis'],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Database',
         'Topic :: Software Development :: Testing'
     ]
